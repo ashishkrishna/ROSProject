eacb7216a0275b1e0c4ba9e7f0fe0107e1d364bd
Ashish Krishna <ashishk@berkeley.edu>
2016-05-20 05:52:14 -0700

    Adding cleanup script

---
+++ garbge.py
@@ -0,0 +1,8 @@
+import shutil
+import os 
+
+shutil.rmtree("/home/ashishk/workspace/build/")
+shutil.rmtree("/home/ashishk/workspace/devel/")
+os.remove("/home/ashishk/workspace/src/CMakeLists.txt")
+
+
