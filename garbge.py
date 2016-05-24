c1497c057d9952fe1d2271e4ad63cd6f8dced8d6
Ashish Krishna <ashishk@berkeley.edu>
2016-05-24 01:56:22 -0700

    adding superfolder

--- garbge.py
+++ garbge.py
@@ -1,8 +1,11 @@
 import shutil
 import os 
+import subprocess
 
 shutil.rmtree("/home/ashishk/workspace/build/")
 shutil.rmtree("/home/ashishk/workspace/devel/")
 os.remove("/home/ashishk/workspace/src/CMakeLists.txt")
+subprocess.call("catkin_make", shell=True)
+
 
 
