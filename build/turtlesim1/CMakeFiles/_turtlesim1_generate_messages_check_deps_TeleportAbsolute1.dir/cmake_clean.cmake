e5508acce8386602cf890fee553a91f9d10ea91f
Ashish Krishna <ashishk@berkeley.edu>
2016-05-25 04:33:54 -0700

    Adding draw sqr

--- build/turtlesim1/CMakeFiles/_turtlesim1_generate_messages_check_deps_TeleportAbsolute1.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_turtlesim1_generate_messages_check_deps_TeleportAbsolute1"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_turtlesim1_generate_messages_check_deps_TeleportAbsolute1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
